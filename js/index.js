64bbca3b395bec67453923ba77d082d0acddf6c0
Megan Maple <91995689+maple-megan333@users.noreply.github.com>
2021-11-30 06:07:25 +0000

    Special header background image and logo SVG complete

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// lazy line painter code
+
+
+(function () {
+
+  document.onreadystatechange = () => {
+
+    if (document.readyState === 'complete') {
+
+      /**
+       * Setup your Lazy Line element.
+       * see README file for more settings
+       */
+
+      let el = document.querySelector('#yellowsvg');
+      let myAnimation = new LazyLinePainter(el, { "ease":"easeLinear",
+      "strokeWidth":1,
+      "strokeOpacity":1,
+      "strokeColor":"#F2B531",
+      "reverse":true});
+      
+      myAnimation.paint();
+    }
+    function setDone() {
+      const logoPt1 = document.querySelector('#yellowsvg-0');
+      logoPt1.classList.add('path-complete');
+      const logoPt2 = document.querySelector('#yellowsvg-1');
+      logoPt2.classList.add('path-complete');
+    }
+    setDone();
+  }
+
+})();
+
+
+
+
+
+
+
+
+
