56d3c28206eafac0f9539d89f7b5c0abee505f1a
Megan Maple <91995689+maple-megan333@users.noreply.github.com>
2021-12-08 01:10:49 +0000

    Landing and about pages complete

--- README.md
+++ README.md
@@ -5,6 +5,13 @@ assets
 intro
 Video by Engin Akyurt from Pexels
 
+Photo by Suzy Hazelwood from Pexels
+https://www.pexels.com/photo/blue-and-red-galaxy-artwork-1629236/
+
+Icons from : 
+
+https://www.freepik.com/free-vector/data-visualization-dynamic-wave-pattern-vector_3226769.htm#page=1&query=line%20art&position=38&from_view=search
+
 
 
 coding tools and resources.
--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-// lazy line painter code
 
+
+// lazy line painter code
 
 
 (function () {
@@ -37,7 +38,22 @@
 
 })();
 
+// simple slider code
 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "inline";  
+  setTimeout(carousel, 9000);    
+}
 
 
 
